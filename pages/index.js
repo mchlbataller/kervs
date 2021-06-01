1c66802179de32a663d67e4a5472b95427556f6c
Michael Bataller <mchlbataller@gmail.com>
2021-06-01 12:19:29 +0800

    Removed styling

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div >
       <Head>
         <title>Happy birthday kervin zoren bonaobra</title>
         <meta name="description" content="made by michael bataller" />
